7638477d867f8d154b79764fd89ca67a7d72ca18
yukixx6 <s1526707DA@s.chibakoudai.jp>
2018-10-15 19:36:35 +0900

    Update left_hand_method

--- sample_program/scripts/left_hand_method.py
+++ sample_program/scripts/left_hand_method.py
@@ -8,45 +8,38 @@ from geometry_msgs.msg import Twist
 from raspimouse_ros_2.msg import LightSensorValues
 from raspimouse_ros_2.msg import MotorFreqs
 
-class Motor:
- def __init__(self):
-  self.motor_sw = 0
-  sub = rospy.Subscriber('lightsensor_values', LightSensorValues, self.callback_ls_val)
-  pub = rospy.Publisher('/raspimouse_on_gazebo/diff_drive_controller/cmd_vel', Twist, queue_size=10)
+def lsf_listener():
+ try:
+  with open("/dev/rtlightsensor0", 'r') as f:
+   date = f.readline().split()
+   date = [int(e) for e in date]
+   motor_vel(date)
+ except:
+  rospy.logerr("cannot open lightsensor file")
 
- def callback_ls_val(self, message):
-  ls_val = message()
-  try:
-   if ls_val.left_side < 1200:
-    self.motor_sw = 1
-   elif ls_val.left_side >= 1200 and ls_val.sum_forward <= 2000:
-    self.motor_sw = 2
-   elif ls_val.ls_val.left_side >= 1200 and ls_val.sum_forward > 2000 and ls_val.right_side <= 1200:
-    self.motor_sw = 3
-   elif ls_val.ls_val.left_side >= 1200 and ls_val.sum_forward > 2000 and ls_val.right_side > 1200:
-    self.motor_sw = 4
-   else:
-    self.motor_sw = 0
-  except: 
-   rospy.logerr("cannot subscrib")
-
- def motor_vel(self):
-  vel = Twist()
-  if self.motor_sw == 0:
-   vel.linear.x = 0.35
-  elif self.motor_sw == 1:
-   vel.angular.z = 3.21
-  elif self.motor_sw == 2:
-   vel.linear.x = 0.35
-  elif self.motor_sw == 3:
-   vel.angular.z = -3.21
-  elif self.motor_sw == 4:
-   vel.angular.z = -6.42
-  pub.publish(vel)
+def motor_vel(date):
+ #print(date)
+ try:
+  with open("/dev/rtmotor_raw_l0", 'w') as lf, open("/dev/rtmotor_raw_r0", 'w') as rf:
+   if date[2] >= 450 and date[0]+date[3] > 2000 and date[1] > 1200:
+    lf.write(str(int(round(-180)))+"\n"), rf.write(str(int(round(180)))+"\n")
+    print("↓")
+   elif date[2] >= 450 and date[0]+date[3]  > 2000 and date[1] <= 1200:
+    lf.write(str(int(round(90)))+"\n"), rf.write(str(int(round(-90)))+"\n")
+    print("→")
+   elif date[2] >= 450 and date[0]+date[3] <= 2000:
+    lf.write(str(int(round(90)))+"\n"), rf.write(str(int(round(90)))+"\n")
+    print("↑")
+   elif date[2] >= 100:
+    lf.write(str(int(round(-45)))+"\n"), rf.write(str(int(round(45)))+"\n")
+    print("←")
+ except:
+  rospy.logerr("cannot write to raw_file")
 
 if __name__ == "__main__":
- rospy.init_node("aaa")
- motor = Motor()
- swfile ="/dev/rtmotoren0"
- motor
- rospy.spin()
+ rospy.init_node("left_hand_method")
+ rate = rospy.Rate(10)
+ while not rospy.is_shutdown():
+  lsf_listener()
+  #motor_vel()
+  rate.sleep()
