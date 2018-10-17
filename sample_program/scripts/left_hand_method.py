c84ee081b46c5eb4e49deee3c937bdd4c6243d8f
yukixx6 <s1526707DA@s.chibakoudai.jp>
2018-10-18 00:47:15 +0900

    Updata left_hand_method

--- sample_program/scripts/left_hand_method.py
+++ sample_program/scripts/left_hand_method.py
@@ -1,73 +1,71 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import sys,math
-import rospy
-
-from geometry_msgs.msg import Twist
-from raspimouse_ros_2.msg import MotorFreqs
+#import sys,math
+import sys, rospy
 
 def lsf_listener():
  try:
   with open("/dev/rtlightsensor0", 'r') as f:
    data = f.readline().split()
    data = [int(e) for e in data]
-   motor_vel(data)
+   print(data)
+   motor_vel(data) 
  except:
-  rospy.logerr("cannot open lightsensor file")
+  rospy.logerr("cannot open rtlightsensor0")
 
 def motor_vel(data):
  try:
-  #if data[2]>=500 and data[0]+data[3]>=1000 and (data[1]>500 and data[2]>500):
-    #status = 4
-  #elif data[2]>=500 and data[0]+data[3]>=1000 and data[1]<500:
-   #status = 3
-  #elif data[2]>=500 and data[0]+data[3]<1000:
-   #status = 2
-  #elif data[2]<500 and 4000>sum(data)>=400:
-   #status = 1
-  #else:
-   #status = 2
-  if data[2] > 3000 or (data[3] > 2500 and data[1] > 2500):
-   status = 4 
-  elif data[0]>150 and data[1]>400:
+  if data[2]<data[0] and data[2]<data[1]:
+   status = 1 
+  elif data[0]+data[3]<data[1]+data[2] and sum(data)<4000:
+   status = 2
+  elif data[1]<data[0] and data[1]<data[2] and data[1]<data[3]:
    status = 3
-  elif data[0]+data[3] < 2000 and data[2] > 500:
-   status = 2
-  elif data[2] < 500 and data[3] < 2000 and sum(data) < 4000:
-   status = 1
   else:
-   status = 2
+   status = 4
+  #rate = rospy.Rate(1)
+  #if not rospy.is_shutdown():
   write_to_file(status)
-  print(status)
+  #print(status)
+   #rate.sleep()
  except:
-  rospy.logerr("cannot status")
+  pass
 
 def write_to_file(data):
  #print(data)
  try:
   with open("/dev/rtmotor_raw_l0", 'w') as lf, open("/dev/rtmotor_raw_r0", 'w') as rf:
    if data == 1:
-    lf.write(str(int(round(409)))+"\n"), rf.write(str(int(round(409)))+"\n")
+    lf.write(str(int(round(-204)))+"\n"), rf.write(str(int(round(204)))+"\n")
     print("right")
    elif data == 2:
-    lf.write(str(int(round(990)))+"\n"), rf.write(str(int(round(990)))+"\n")
+    lf.write(str(int(round(495)))+"\n"), rf.write(str(int(round(495)))+"\n")
     print("go")
    elif data == 3:
-    lf.write(str(int(round(409)))+"\n"), rf.write(str(int(round(409)))+"\n")
+    lf.write(str(int(round(204)))+"\n"), rf.write(str(int(round(-204)))+"\n")
     print("left")
    elif data == 4:
-    lf.write(str(int(round(817)))+"\n"), rf.write(str(int(round(-814)))+"\n")
+    lf.write(str(int(round(409)))+"\n"), rf.write(str(int(round(-409)))+"\n")
     print("U turn")
-   elif data == 0:
-    lf.write(str(int(round(0)))+"\n"), rf.write(str(int(round(0)))+"\n")
  except:
-  rospy.logerr("cannot write to raw_file")
+  rospy.logerr("cannot write to rtmotor_raw_*")
+
+def reset():
+ try:
+  with open("/dev/rtmotor_raw_l0", 'w') as lf, open("/dev/rtmotor_raw_r0", 'w') as rf:
+   lf.write(str(int(round(0)))+"\n"), rf.write(str(int(round(0)))+"\n")
+   #print("reset")
+ except:
+  rospy.logerr("cannot reset")
 
 if __name__ == "__main__":
  rospy.init_node("left_hand_method")
- #rate = rospy.Rate(1)
+ rate = rospy.Rate(1)
  while not rospy.is_shutdown():
-  lsf_listener()
-  #rate.sleep()
- #rospy.spin()
+  if not rospy.is_shutdown():
+   lsf_listener()
+   rate.sleep()
+  if not rospy.is_shutdown():
+   reset()
+   rate.sleep()
