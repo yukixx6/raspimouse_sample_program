089ab90c88fca8ebabb2942432d4a180bf151d37
yukixx6 <s1526707DA@s.chibakoudai.jp>
2018-10-23 14:53:27 +0900

    Update left_hand.py

--- sample_program/scripts/left_hand.py
+++ sample_program/scripts/left_hand.py
@@ -8,7 +8,8 @@ class Left_hand():
  def __init__(self):
   self.sub = rospy.Subscriber('/lightsensors', LightSensorValues, self.callback_ls)
   self.time = 0.0
-  self.now = 0.0
+  self.ls_time = 0.0
+  self.write_time = 0.0
   self.status = 0
   self.ls_val = [0, 0, 0, 0, 0, 0]
 
@@ -16,15 +17,12 @@ class Left_hand():
   self.time = rospy.get_time()
 
  def callback_ls(self, value):
-  #self.motor_status(value)
-  self.time_set()
-  #self.now = self.time
-  #print(value)
-  if self.now+0.8 >= self.time:
+  if self.ls_time + 0.8 <= self.time:
    self.ls_val = value
-   self.motor_status(self.ls_val)
-  else:
-   self.motor_status(value)
+   self.now = self.time
+  self.time_set()
+  self.motor_status(self.ls_val)
+  #print(self.ls_val)
 
  def motor_status(self, data):
   try:
@@ -36,46 +34,45 @@ class Left_hand():
     self.status = 3
    else:
     self.status = 4
-
-   self.time_set()
-   if self.now+1.0 <= self.time:
-    self.reset()
-   else:
-    self.write_to_file(self.status)
-    #self.time_set()
-
+   self.write_to_file(self.status)
   except:
    rospy.logerr("cannot status")
+  #print(self.status)
 
  def write_to_file(self, data):
-  self.time_set()
-  self.now = self.time
-  if self.now+1.0 <= self.time: 
-   try:
+  try:
+   self.time_set()
+   self.ls_time = self.time
+   self.write_time = self.time
+   while self.write_time+1.0 >= self.time:
     with open("/dev/rtmotor_raw_l0", 'w') as lf, open("/dev/rtmotor_raw_r0", 'w') as rf:
      if data == 1:
       lf.write(str(int(round(-204)))+"\n"), rf.write(str(int(round(204)))+"\n")
-      print("right")
+      #print("right")
      elif data == 2:
       lf.write(str(int(round(495)))+"\n"), rf.write(str(int(round(495)))+"\n")
       print("go")
      elif data == 3:
       lf.write(str(int(round(204)))+"\n"), rf.write(str(int(round(-204)))+"\n")
-      print("left")
+      #print("left")
      elif data == 4:
       lf.write(str(int(round(409)))+"\n"), rf.write(str(int(round(-409)))+"\n")
-      print("U turn")
+      #print("U turn")
     self.time_set()
-   except:
-    rospy.logerr("cannot write to rtmotor_raw_*")
-   
+    #print(self.write_time+1.0-self.time)
+   self.write_time = self.time
+   while self.write_time+0.5>=self.time:
+    self.reset()
+    self.time_set()
+   self.write_time = self.time
+  except:
+   rospy.logerr("cannot write to rtmotor_raw_*")
+
  def reset(self):
   try:
    with open("/dev/rtmotor_raw_l0", 'w') as lf, open("/dev/rtmotor_raw_r0", 'w') as rf:
     lf.write(str(int(round(0)))+"\n"), rf.write(str(int(round(0)))+"\n")
-     #print("reset")
-   self.time_set()
-   self.now = self.time
+    #print("reset")
   except:
    pass
 
@@ -83,6 +80,8 @@ if __name__ == "__main__":
  rospy.init_node("left_hand_method")
  l = Left_hand()
  l.time_set()
- l.now = l.time
- l
+ l.ls_time = l.time
+ l.write_time = l.time
+  #l.reset()
+  #rospy.spin()
  rospy.spin()
